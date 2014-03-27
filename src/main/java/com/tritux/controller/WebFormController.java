d697f14948c7889254c27960fd633a56860fce2f
Oussama Denden <denden.oussama@gmail.com>
2014-03-27 12:50:21 +0100

    adding web services

--- src/main/java/com/tritux/controller/WebFormController.java
+++ src/main/java/com/tritux/controller/WebFormController.java
@@ -1,7 +1,15 @@
 package com.tritux.controller;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,112 +17,122 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tritux.domain.TravelTrip;
 import com.tritux.service.TravelTripService;
- 
+
 @Controller
 public class WebFormController {
-     
-    private TravelTripService travelTripService;
- 
-    @Autowired
-    public WebFormController(TravelTripService travelTripService){
-        this.travelTripService = travelTripService;
-    }
- 
-    private boolean firstTime = true;
- 
-    @RequestMapping(value = "/myTravels", method = RequestMethod.GET)
-    public String displaySortedTravelTrips(Model model) {
-        if(firstTime){
-            createDummyTravelTrips();
-            firstTime = false;
-        }
-        model.addAttribute("travelTripList", travelTripService.findAll());
-        return "index";
-    }
- 
-    @RequestMapping(value = "/addTravelTripForm", method = RequestMethod.GET)
-    public String addTravelTripForm(Model model){
-        model.addAttribute("newTravelTrip", new TravelTrip());
-        return "addTravelTrip";
-    }
- 
-    @RequestMapping(value = "/addTravelTrip", method = RequestMethod.POST)
-    public String registerNewTravelTrip(
-            @Valid @ModelAttribute("newTravelTrip") TravelTrip newTravelTrip,
-            BindingResult result, Model model) {                
-         
-        if (!result.hasErrors()) {          
-            travelTripService.addTravelTrip(newTravelTrip);
-            model.addAttribute("travelTripList", travelTripService.findAll());
-            return "redirect:/";
-        } else {
-            return "addTravelTrip";
- 
-        }       
-    }
- 
-    @RequestMapping(value="/{tripID}/deleteTravelTrip", method=RequestMethod.GET)
-    public String deleteTravelTrip(@PathVariable("tripID") String tripID, Model model){     
- 
-        travelTripService.delete(Integer.parseInt(tripID));     
-        model.addAttribute("travelTripList", travelTripService.findAll());      
-        return "redirect:/";
-    }
- 
-    @RequestMapping(value="/{tripID}/editTravelTripForm", method=RequestMethod.GET)
-    public String ediTravelTripForm(@PathVariable("tripID") String tripID, Model model){        
-        
-        TravelTrip tempTravelTrip = travelTripService.findById(Integer.parseInt(tripID));
- 
-        if(tempTravelTrip == null){
-            return "index";
-        }
- 
-        model.addAttribute("editTravelTrip", tempTravelTrip);
- 
-        return "/editTravelTrip";
-    }
- 
-    @RequestMapping(value="/{tripID}/editTravelTrip", method=RequestMethod.POST)
-    public String ediTravelTrip(@PathVariable("tripID") String tripID,
-            @Valid @ModelAttribute("editTravelTrip") TravelTrip editTravelTrip,
-            BindingResult result, Model model){ 
-         
-        if (!result.hasErrors()) {
- 
-            travelTripService.update(Integer.parseInt(tripID), editTravelTrip);
-            model.addAttribute("travelTripList", travelTripService.findAll());
-            return "redirect:/";
-        } else {
-            //model.addAttribute("travelTripList", travelTripService.findAll());
-            return "editTravelTrip";
- 
-        }   
-    }
- 
-    @RequestMapping(value="/{tripID}/detailsTravelTrip", method=RequestMethod.GET)
-    public String detailsTravelTrip(@PathVariable("tripID") String tripID, Model model){
- 
-        TravelTrip tempTravelTrip = travelTripService.findById(Integer.parseInt(tripID));
- 
-        model.addAttribute("detailsTravelTrip", tempTravelTrip);
- 
-        return "detailsTravelTrip";
-    }              
- 
-    private void createDummyTravelTrips() {
-        TravelTrip temp = new TravelTrip();
- 
-        temp.setCountry("Pakistan");
-        temp.setCity("Lahore");
-        temp.setFromDate("19/06/2012");
-        temp.setToDate("27/06/2012");
-        temp.setBusiness(false);
- 
-        //travelTripDao.register(temp);
-        travelTripService.addTravelTrip(temp);
-    }
+
+	private TravelTripService travelTripService;
+
+	@Autowired
+	public WebFormController(TravelTripService travelTripService) {
+		this.travelTripService = travelTripService;
+	}
+
+	@RequestMapping(value = "/myTravels", method = RequestMethod.GET)
+	public String displaySortedTravelTrips(Model model) {
+		model.addAttribute("travelTripList", travelTripService.findAll());
+		return "index";
+	}
+
+	/**
+	 * fetch all travels trips and show them.
+	 * 
+	 * @param model
+	 *            model.
+	 * @return page name.
+	 */
+	@RequestMapping(value = "/export", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<String> exportTravelTrips(Model model) {
+		List<TravelTrip> travelTrips = travelTripService.findAll();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/json; charset=utf-8");
+		if (travelTrips == null) {
+			return new ResponseEntity<String>(headers, HttpStatus.NO_CONTENT);
+		}
+		Logger.getLogger(WebFormController.class.toString()).log(Level.INFO,
+				"Exporting " + travelTrips.size() + " travel trips ");
+		return new ResponseEntity<String>(TravelTrip.toJsonArray(travelTrips),
+				headers, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/addTravelTripForm", method = RequestMethod.GET)
+	public String addTravelTripForm(Model model) {
+		model.addAttribute("newTravelTrip", new TravelTrip());
+		return "addTravelTrip";
+	}
+
+	@RequestMapping(value = "/addTravelTrip", method = RequestMethod.POST)
+	public String registerNewTravelTrip(
+			@Valid @ModelAttribute("newTravelTrip") TravelTrip newTravelTrip,
+			BindingResult result, Model model) {
+
+		if (!result.hasErrors()) {
+			travelTripService.addTravelTrip(newTravelTrip);
+			model.addAttribute("travelTripList", travelTripService.findAll());
+			return "redirect:/";
+		} else {
+			return "addTravelTrip";
+
+		}
+	}
+
+	@RequestMapping(value = "/{tripID}/deleteTravelTrip", method = RequestMethod.GET)
+	public String deleteTravelTrip(@PathVariable("tripID") String tripID,
+			Model model) {
+
+		travelTripService.delete(Integer.parseInt(tripID));
+		model.addAttribute("travelTripList", travelTripService.findAll());
+		return "redirect:/";
+	}
+
+	@RequestMapping(value = "/{tripID}/editTravelTripForm", method = RequestMethod.GET)
+	public String ediTravelTripForm(@PathVariable("tripID") String tripID,
+			Model model) {
+
+		TravelTrip tempTravelTrip = travelTripService.findById(Integer
+				.parseInt(tripID));
+
+		if (tempTravelTrip == null) {
+			return "index";
+		}
+
+		model.addAttribute("editTravelTrip", tempTravelTrip);
+
+		return "/editTravelTrip";
+	}
+
+	@RequestMapping(value = "/{tripID}/editTravelTrip", method = RequestMethod.POST)
+	public String ediTravelTrip(@PathVariable("tripID") String tripID,
+			@Valid @ModelAttribute("editTravelTrip") TravelTrip editTravelTrip,
+			BindingResult result, Model model) {
+
+		if (!result.hasErrors()) {
+
+			travelTripService.update(Integer.parseInt(tripID), editTravelTrip);
+			model.addAttribute("travelTripList", travelTripService.findAll());
+			return "redirect:/";
+		} else {
+			// model.addAttribute("travelTripList",
+			// travelTripService.findAll());
+			return "editTravelTrip";
+
+		}
+	}
+
+	@RequestMapping(value = "/{tripID}/detailsTravelTrip", method = RequestMethod.GET)
+	public String detailsTravelTrip(@PathVariable("tripID") String tripID,
+			Model model) {
+
+		TravelTrip tempTravelTrip = travelTripService.findById(Integer
+				.parseInt(tripID));
+
+		model.addAttribute("detailsTravelTrip", tempTravelTrip);
+
+		return "detailsTravelTrip";
+	}
 }
--- src/main/java/com/tritux/domain/TravelTrip.java
+++ src/main/java/com/tritux/domain/TravelTrip.java
@@ -1,6 +1,9 @@
 package com.tritux.domain;
 import java.io.Serializable;
- 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,93 +12,141 @@ import javax.persistence.NamedQuery;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
- 
+
 import org.hibernate.validator.constraints.NotEmpty;
- 
+
+import flexjson.JSONDeserializer;
+import flexjson.JSONSerializer;
+
 @Entity
 @NamedQueries({
-    @NamedQuery(name="TravelTrip.findAll",
-                query="SELECT tt FROM TravelTrip tt"),
-    @NamedQuery(name="TravelTrip.findByTripType",
-                query="SELECT tt FROM TravelTrip tt WHERE tt.business = :business")
-}) 
+		@NamedQuery(name = "TravelTrip.findAll", query = "SELECT tt FROM TravelTrip tt"),
+		@NamedQuery(name = "TravelTrip.findByTripType", query = "SELECT tt FROM TravelTrip tt WHERE tt.business = :business") })
 public class TravelTrip implements Serializable {
-     
-    private static final long serialVersionUID = -8909475486646388460L;
- 
-    @Id
-    @GeneratedValue
-    private int id;
- 
-    @NotNull
-    @Size(min = 5, max = 25)
-    @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
-    private String country;
- 
-    @NotNull
-    @Size(min = 5, max = 25)
-    @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
-    private String city;
- 
-    @NotNull
-    @NotEmpty
-    private String fromDate;
- 
-    @NotNull
-    @NotEmpty
-    private String toDate;
- 
-    private boolean business;
- 
-    public TravelTrip() {
- 
-    }
- 
-    public int getId() {
-        return id;
-    }
- 
-    public void setId(int id) {
-        this.id = id;
-    }
- 
-    public String getCountry() {
-        return country;
-    }
- 
-    public void setCountry(String country) {
-        this.country = country;
-    }
- 
-    public String getCity() {
-        return city;
-    }
- 
-    public void setCity(String city) {
-        this.city = city;
-    }
- 
-    public String getFromDate() {
-        return fromDate;
-    }
- 
-    public void setFromDate(String fromDate) {
-        this.fromDate = fromDate;
-    }
- 
-    public String getToDate() {
-        return toDate;
-    }
- 
-    public void setToDate(String toDate) {
-        this.toDate = toDate;
-    }
- 
-    public boolean isBusiness() {
-        return business;
-    }
- 
-    public void setBusiness(boolean business) {
-        this.business = business;
-    }
+
+	private static final long serialVersionUID = -8909475486646388460L;
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@NotNull
+	@Size(min = 5, max = 25)
+	@Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
+	private String country;
+
+	@NotNull
+	@Size(min = 5, max = 25)
+	@Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
+	private String city;
+
+	@NotNull
+	@NotEmpty
+	private String fromDate;
+
+	@NotNull
+	@NotEmpty
+	private String toDate;
+
+	private boolean business;
+
+	public TravelTrip() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(String fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public String getToDate() {
+		return toDate;
+	}
+
+	public void setToDate(String toDate) {
+		this.toDate = toDate;
+	}
+
+	public boolean isBusiness() {
+		return business;
+	}
+
+	public void setBusiness(boolean business) {
+		this.business = business;
+	}
+
+	/**
+	 * Serialize a travel trip object to a string.
+	 * 
+	 * @return string representation of the travel trip object.
+	 */
+	public String toJson() {
+		return new JSONSerializer().exclude("*.class").serialize(this);
+	}
+
+	/**
+	 * 
+	 * Transform a string into a travel trip object.
+	 * 
+	 * @param json
+	 *            string representation of the travel trip.
+	 * @return {@link TravelTrip}
+	 */
+	public static TravelTrip fromJsonToActivityBranch(String json) {
+		return new JSONDeserializer<TravelTrip>().use(null, TravelTrip.class)
+				.deserialize(json);
+	}
+
+	/**
+	 * Serialize a collection of travel trip object to a string.
+	 * 
+	 * @param collection
+	 *            the collection to be translated to a string.
+	 * @return string representation of the travel trip collection.
+	 */
+	public static String toJsonArray(Collection<TravelTrip> collection) {
+		return new JSONSerializer().exclude("*.class").serialize(collection);
+	}
+
+	/**
+	 * 
+	 * Transform a string into a travel trip collection.
+	 * 
+	 * @param json
+	 *            string representation of the collection of travel trip.
+	 * @return collection of {@link TravelTrip}
+	 */
+	public static Collection<TravelTrip> fromJsonArrayToActivityBranches(
+			String json) {
+		return new JSONDeserializer<List<TravelTrip>>()
+				.use(null, ArrayList.class).use("values", TravelTrip.class)
+				.deserialize(json);
+	}
 }
